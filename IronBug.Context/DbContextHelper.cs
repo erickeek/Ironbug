f093344a8adb48fc1b5a13398ded479cb09518d9
Erick <erickeek@hotmail.com>
2019-08-23 14:23:19 -0300

    Add project files.

---
+++ IronBug.Context/DbContextHelper.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace IronBug.Context
+{
+    public static class DbContextHelper
+    {
+        public static TDbContext BulkInsert<TDbContext, TEntity>(this TDbContext context, IEnumerable<TEntity> entities, DbContextOptions options = null)
+            where TDbContext : DbContext
+            where TEntity : class
+        {
+            var count = 0;
+
+            options = options ?? new DbContextOptions();
+            context.Configuration.AutoDetectChangesEnabled = options.AutoDetectChangesEnabled;
+            context.Configuration.ValidateOnSaveEnabled = options.ValidateOnSaveEnabled;
+
+            foreach (var entity in entities)
+            {
+                count++;
+                context.Set<TEntity>().Add(entity);
+
+                if (count % options.CommitCount == 0)
+                {
+                    context.SaveChanges();
+                    context.Dispose();
+                    context = Activator.CreateInstance<TDbContext>();
+                    context.Configuration.AutoDetectChangesEnabled = options.AutoDetectChangesEnabled;
+                    context.Configuration.ValidateOnSaveEnabled = options.ValidateOnSaveEnabled;
+                }
+            }
+
+            context.SaveChanges();
+            return context;
+        }
+
+        public static void Truncate<TEntity>(this DbContext context) where TEntity : class
+        {
+            context.Database.ExecuteSqlCommand($"TRUNCATE TABLE {typeof(TEntity).Name}");
+        }
+    }
+}
---
+++ IronBug.Context/DbContextOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace IronBug.Context
+{
+    public class DbContextOptions
+    {
+        public DbContextOptions()
+        {
+            CommitCount = 1000;
+            AutoDetectChangesEnabled = false;
+            ValidateOnSaveEnabled = false;
+        }
+
+        public int CommitCount { get; set; }
+        public bool AutoDetectChangesEnabled { get; set; }
+        public bool ValidateOnSaveEnabled { get; set; }
+    }
+}
