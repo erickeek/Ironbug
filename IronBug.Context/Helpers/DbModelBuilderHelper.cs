cfc1541ac1a42968a51f3277d074a56567cb16c7
Erick <erickeek@hotmail.com>
2019-08-26 16:40:33 -0300

    add extensio method to add all type configurations

---
+++ IronBug.Context/Helpers/DbModelBuilderHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Reflection;
+
+namespace IronBug.Context.Helpers
+{
+    public static class DbModelBuilderHelper
+    {
+        public static void RegisterAllTypeConfigurations(this DbModelBuilder modelBuilder, Assembly assembly)
+        {
+            var typesToRegister = assembly.GetTypes()
+                .Where(type => !string.IsNullOrEmpty(type.Namespace))
+                .Where(type => type.BaseType != null && type.BaseType.IsGenericType && type.BaseType.GetGenericTypeDefinition() == typeof(EntityTypeConfiguration<>));
+
+            foreach (var configurationInstance in typesToRegister.Select(Activator.CreateInstance))
+            {
+                modelBuilder.Configurations.Add((dynamic)configurationInstance);
+            }
+        }
+    }
+}
