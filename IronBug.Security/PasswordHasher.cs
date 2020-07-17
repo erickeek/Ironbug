9a308210b2223e08d1bca1ce1c47cd9d3bb92387
Erick <erickeek@hotmail.com>
2020-07-16 21:34:00 -0300

    add password helpers

---
+++ IronBug.Security/PasswordHasher.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace IronBug.Security
+{
+    public interface IPasswordHasher
+    {
+        string HashPassword(string password);
+        bool VerifyHashedPassword(string hashedPassword, string password);
+    }
+
+    public sealed class PasswordHasher : IPasswordHasher
+    {
+        private byte Version => 1;
+        private int SaltSize { get; } = 128 / 8; // 128 bits
+        private HashAlgorithmName HashAlgorithmName { get; } = HashAlgorithmName.SHA256;
+
+        public string HashPassword(string password)
+        {
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+
+            var salt = GenerateSalt(SaltSize);
+            var hash = HashPasswordWithSalt(password, salt);
+
+            var inArray = new byte[1 + SaltSize + hash.Length];
+            inArray[0] = Version;
+            Buffer.BlockCopy(salt, 0, inArray, 1, SaltSize);
+            Buffer.BlockCopy(hash, 0, inArray, 1 + SaltSize, hash.Length);
+
+            return Convert.ToBase64String(inArray);
+        }
+
+        public bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            if (password == null)
+                throw new ArgumentNullException(nameof(password));
+
+            if (hashedPassword == null)
+                return false;
+
+            Span<byte> numArray = Convert.FromBase64String(hashedPassword);
+            if (numArray.Length < 1)
+                return false;
+
+            var version = numArray[0];
+            if (version > Version)
+                return false;
+
+            var salt = numArray.Slice(1, SaltSize).ToArray();
+            var bytes = numArray.Slice(1 + SaltSize).ToArray();
+
+            var hash = HashPasswordWithSalt(password, salt);
+
+            return FixedTimeEquals(hash, bytes);
+        }
+
+        private byte[] HashPasswordWithSalt(string password, byte[] salt)
+        {
+            byte[] hash;
+            using (var hashAlgorithm = HashAlgorithm.Create(HashAlgorithmName.Name))
+            {
+                var input = Encoding.UTF8.GetBytes(password);
+                hashAlgorithm.TransformBlock(salt, 0, salt.Length, salt, 0);
+                hashAlgorithm.TransformFinalBlock(input, 0, input.Length);
+                hash = hashAlgorithm.Hash;
+            }
+
+            return hash;
+        }
+
+        private static byte[] GenerateSalt(int byteLength)
+        {
+            using (var cryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                var data = new byte[byteLength];
+                cryptoServiceProvider.GetBytes(data);
+                return data;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        public static bool FixedTimeEquals(byte[] left, byte[] right)
+        {
+            if (left.Length != right.Length)
+            {
+                return false;
+            }
+
+            var length = left.Length;
+            var accum = 0;
+
+            for (var i = 0; i < length; i++)
+            {
+                accum |= left[i] - right[i];
+            }
+
+            return accum == 0;
+        }
+    }
+}
---
+++ IronBug.Security/PasswordHelper.cs
@@ -0,0 +1,17 @@
+﻿namespace IronBug.Security
+{
+    public static class PasswordHelper
+    {
+        private static readonly PasswordHasher PasswordHasher = new PasswordHasher();
+
+        public static string Encrypt(this string password)
+        {
+            return PasswordHasher.HashPassword(password);
+        }
+
+        public static bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            return PasswordHasher.VerifyHashedPassword(hashedPassword, password);
+        }
+    }
+}
