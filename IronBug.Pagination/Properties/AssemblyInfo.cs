7cd55584cefe513664dca3459a1fe588b5c0bf92
Erick <erickeek@hotmail.com>
2020-07-31 16:19:32 -0300

    add .NET Standard project for Pagination

--- IronBug.Pagination/Properties/AssemblyInfo.cs
+++ IronBug.Pagination/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3cc20403-a4eb-4257-8ddb-e90f4947d218")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
