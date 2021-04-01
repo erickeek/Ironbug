d2cfe6ee38d89c6265393aa13bca985562487602
Erick <erickeek@hotmail.com>
2021-04-01 11:34:28 -0300

    add content versioned to .net core

---
+++ IronBug.Web.Portable/Helpers/ExtendedUrlHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IronBug.Web.Helpers
+{
+    public static class ExtendedUrlHelper
+    {
+        public static string ContentVersioned(this IUrlHelper helper, string contentPath)
+        {
+            var version = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version ?? "1.0";
+            var versionedContentPath = $"{contentPath}?v={version}";
+            return helper.Content(versionedContentPath);
+        }
+    }
+}
