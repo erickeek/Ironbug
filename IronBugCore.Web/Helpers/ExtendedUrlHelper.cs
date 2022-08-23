b8ce0988ce9ddbedfe40829a102088993a7d4e47
erick <erickeek@hotmail.com>
2022-08-22 23:09:44 -0300

    add web helpers for .net core

---
+++ IronBugCore.Web/Helpers/ExtendedUrlHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IronBugCore.Web.Helpers;
+
+public static class ExtendedUrlHelper
+{
+    public static string ContentVersioned(this IUrlHelper helper, string contentPath)
+    {
+        var version = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version ?? "1.0";
+        var versionedContentPath = $"{contentPath}?v={version}";
+        return helper.Content(versionedContentPath);
+    }
+}
---
+++ IronBugCore.Web/Helpers/UrlHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IronBugCore.Web.Helpers;
+
+public static class UrlHelper
+{
+    public static string AbsoluteContent(this IUrlHelper url, string contentPath)
+    {
+        var request = url.ActionContext.HttpContext.Request;
+        return new Uri(new Uri($"{request.Scheme}://{request.Host.Value}"), url.Content(contentPath)).ToString();
+    }
+}
